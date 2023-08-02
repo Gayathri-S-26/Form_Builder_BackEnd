3e9f2d4b2d03a50f5f512bc9d8a9fb6c2d887720
Gayathri-S-26 <85394882+Gayathri-S-26@users.noreply.github.com>
2023-08-02 16:37:14 +0530

    Update Form.js

--- server/models/Form.js
+++ server/models/Form.js
@@ -1,10 +1,10 @@
-//models/Form.js
+// models/Form.js
 
 const mongoose = require('mongoose');
 
 const formSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  elements: { type: Array, required: true },
+  elements: [{ type: mongoose.Schema.Types.Mixed }], // Always store elements as an array
 });
 
 const Form = mongoose.model('Form', formSchema);
